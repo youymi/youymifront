28dd571797df55bf571fc7a9369549e69eee6926
youymi <youymi@163.com>
2015-01-21 15:30:42 +0800

    fix

--- commonjs/commonjs.js
+++ commonjs/commonjs.js
@@ -1,9 +1,9 @@
 function getElementsByClassName(className, tagName) {
-	//var now = new Date().getTime();
+    //var now = new Date().getTime();
     var ele = [],
         all = document.getElementsByTagName(tagName || "*");
     for (var i = 0; i < all.length; i++) {
-    	// 	/(\s|^)pager(\s|$)/g
+        //  /(\s|^)pager(\s|$)/g
         if (all[i].className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))) {
             ele[ele.length] = all[i];
         }
@@ -12,37 +12,63 @@ function getElementsByClassName(className, tagName) {
     return ele;
 }
 
-function findElementsByClassName(parentObj,className) {
-	var now = new Date().getTime();
-    	var ele = [];
-    	if (parentObj && parentObj.hasChildNodes()) {
-    		var childden = parentObj.childNodes;
-    		var node = null;
-    		for (var i = 0; i < childden.length; i++) {
-    			 node = childden[i];
-    			 if (node.nodeType == 1) {
-    			 	if (node.hasAttribute("class") &&node.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))) {
-    			 		ele.push(node);
-    			 	}
-    			 	//element node
-    			 	if (node.hasChildNodes()) {
-    			 		ele = ele.concat(findElementsByClassName(node,className));
-    			 	}
-    			 }
-    		}
+function findElementsByClassName(parentObj, className) {
+    var now = new Date().getTime();
+    var ele = [];
+    if (parentObj && parentObj.hasChildNodes()) {
+        var childden = parentObj.childNodes;
+        var node = null;
+        for (var i = 0; i < childden.length; i++) {
+            node = childden[i];
+            if (node.nodeType == 1) {
+                if (node.hasAttribute("class") && node.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))) {
+                    ele.push(node);
+                }
+                //element node
+                if (node.hasChildNodes()) {
+                    ele = ele.concat(findElementsByClassName(node, className));
+                }
+            }
+        }
 
-    	}  
+    }
 
-    	return ele;
- 
+    return ele;
+
 }
 
 
- function setSelectedItemByValue(objSelect, value) {  
-        for(var i=0;i<objSelect.options.length;i++) {  
-            if(objSelect.options[i].value == value) {  
-                objSelect.options[i].selected = true;  
-                break;  
-            }  
-        }  
-    }  
+function setSelectedItemByValue(objSelect, value) {
+    for (var i = 0; i < objSelect.options.length; i++) {
+        if (objSelect.options[i].value == value) {
+            objSelect.options[i].selected = true;
+            break;
+        }
+    }
+}
+
+function HTMLEnCode(str) {
+    var s = "";
+    if (str.length == 0) return "";
+    s = str.replace(/&/g, "&gt;");
+    s = s.replace(/ </g, "&lt;");
+    s = s.replace(/>/g, "&gt;");
+    s = s.replace(/    /g, "&nbsp;");
+    s = s.replace(/\'/g, "'");
+    s = s.replace(/\"/g, "&quot;");
+    s = s.replace(/\n/g, " <br>");
+    return s;
+}
+
+function HTMLDeCode(str) {
+    var s = "";
+    if (str.length == 0) return "";
+    s = str.replace(/&gt;/g, "&");
+    s = s.replace(/&lt;/g, " <");
+    s = s.replace(/&gt;/g, ">");
+    s = s.replace(/&nbsp;/g, "    ");
+    s = s.replace(/'/g, "\'");
+    s = s.replace(/&quot;/g, "\"");
+    s = s.replace(/ <br>/g, "\n");
+    return s;
+}
